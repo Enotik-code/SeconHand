f4d380d5f7a58bdbbe65473bc3be9b89ffb21073
alekseykarpeychyk <karpeychyk@mail.ru>
2020-08-20 17:33:37 +0300

    20/08/2020

--- src/main/java/by/seconhand/Application.java
+++ src/main/java/by/seconhand/Application.java
@@ -1,5 +1,5 @@
 package by.seconhand;
-
+// a
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
