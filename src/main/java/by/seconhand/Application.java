493ae9a7dcd9e9d0a05f15ae8910ae32259d9564
Antony <fvantop@gmail.com>
2020-08-20 17:42:57 +0300

    20/08 telegram added

--- src/main/java/by/seconhand/Application.java
+++ src/main/java/by/seconhand/Application.java
@@ -2,11 +2,7 @@ package by.seconhand;
 // IDI NAH
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.telegram.telegrambots.ApiContextInitializer;
-
-import java.io.IOException;
-import java.text.ParseException;
+//SAM idi
 
 @SpringBootApplication
 public class Application {
