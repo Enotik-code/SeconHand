8bfb7e3637a7e56c89b6ea5d95f0704c5b8ef1ce
alekseykarpeychyk <karpeychyk@mail.ru>
2020-08-20 17:40:18 +0300

    20/08/2020

--- src/main/java/by/seconhand/Application.java
+++ src/main/java/by/seconhand/Application.java
@@ -1,5 +1,5 @@
 package by.seconhand;
-// a
+// IDI NAH
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
